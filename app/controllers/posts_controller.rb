d37109efdb7ce12a5f66e887610ae385184069b1
suwakouhei <s.k.293758@icloud.com>
2022-02-26 18:27:05 +0900

    includeメソッドを利用してパフォーマンス維持

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
   before_action :move_to_index, except: [:index, :show]
 
   def index
-    @posts = Post.all
+    @posts = Post.includes(:user)
   end
 
   def new
