e35ebfa2b0d2f0e0f225343223c7e48646903ddc
suwakouhei <s.k.293758@icloud.com>
2022-02-26 20:27:37 +0900

    id名の変更

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,6 +6,6 @@ class CommentsController < ApplicationController
 
   private
   def comment_params
-    params.require(:comment).permit(:text).merge(user_id: current_user.id, tweet_id: params[:post_id])
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, post_id: params[:post_id])
   end
 end
