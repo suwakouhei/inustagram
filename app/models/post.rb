c10e39168859693265dd69399f73976750ffcdd3
suwakouhei <s.k.293758@icloud.com>
2022-02-27 18:28:57 +0900

    モデル内にビジネスロジックを記述

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,13 @@ class Post < ApplicationRecord
   validates :text, presence: true
   belongs_to :user
   has_many :comments
+
+  def self.search(search)
+    if search != ""
+      Post.where('text LIKE(?)', "%#{search}%") 
+    else
+      Post.all
+    end
+  end
+
 end
